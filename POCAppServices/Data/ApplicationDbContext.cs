a01bdad7623e2107a81cf92b85d8a58fb066857c
Admin <Admin@DESKTOP-D5MT2DG>
2026-06-22 12:31:52 +0530

    Add project files.

---
+++ POCAppServices/Data/ApplicationDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace POCAppServices.Data
+{
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options)
+    {
+    }
+}
